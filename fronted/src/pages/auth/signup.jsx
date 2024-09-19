19313ed845c7f8fbb5a5b0a45f612159dd9eeee3
rajsekhar77 <rajasekhar.b5401@gmail.com>
2024-09-19 15:35:34 +0530

    functionality added for signup page

--- fronted/src/pages/auth/signup.jsx
+++ fronted/src/pages/auth/signup.jsx
@@ -1,7 +1,65 @@
+import Commonform from "@/components/commonComponents/form";
+import { registerFormControls } from "@/config";
+import { registerUser } from "@/features/auth/authSlice";
+import { useToast } from "@/hooks/use-toast";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+
+const initialState = {
+  userName: "",
+  email: "",
+  password: "",
+};
+
 function AuthRegister() {
+  const [formData, setFormData] = useState(initialState);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { toast } = useToast();
+
+  function onSubmit(event) {
+    event.preventDefault();
+    dispatch(registerUser(formData)).then((data) => {
+      if (data?.payload?.success) {
+        toast({
+          title: data?.payload?.message,
+        });
+        navigate("/auth/login");
+      } else {
+        toast({
+          title: data?.payload?.message,
+          variant: "destructive",
+        });
+      }
+      console.log(data);
+    });
+  }
+
   return (
-    <div>
-      Signup
+    <div className="mx-auto w-full max-w-md space-y-6">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold tracking-tight text-foreground">
+          Create a new Account
+        </h1>
+        <p>
+          Already have an account
+          <Link
+            className="font-medium ml-2 text-primary hover:underline"
+            to="/auth/login"
+          >
+            Login
+          </Link>
+        </p>
+      </div>
+      <Commonform
+        formControls={registerFormControls}
+        buttonText={"Sign Up"}
+        formData={formData}
+        setFormData={setFormData}
+        onSubmit={onSubmit}
+      />
     </div>
   );
 }
