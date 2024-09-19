63630b14335dc032e276c662418aed57cdf68f63
rajsekhar77 <rajasekhar.b5401@gmail.com>
2024-09-19 15:10:10 +0530

    ui completed for the login page

--- fronted/src/main.jsx
+++ fronted/src/main.jsx
@@ -5,11 +5,13 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import store from "./store/store.js";
 import { Provider } from "react-redux";
+import { Toaster } from "./components/ui/toaster.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Provider store={store}>
       <App />
+      <Toaster />
     </Provider>
   </BrowserRouter>
 );
--- fronted/src/pages/auth/login.jsx
+++ fronted/src/pages/auth/login.jsx
@@ -1,7 +1,37 @@
+import Commonform from "@/components/commonComponents/form";
+import { loginFormControls } from "@/config";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const initialState = {
+  email: "",
+  password: "",
+};
+
 function AuthLogin() {
+  const [formData, setFormData] = useState(initialState);
   return (
-    <div>
-      Login
+    <div className="mx-auto w-full max-w-md space-y-6">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold tracking-tight text-foreground">
+          Login in to your account
+        </h1>
+        <p>
+          Don't have an account
+          <Link
+            className="font-medium ml-2 text-primary hover:underline"
+            to="/auth/signup"
+          >
+            Register
+          </Link>
+        </p>
+      </div>
+      <Commonform
+        formControls={loginFormControls}
+        buttonText={"Login In"}
+        formData={formData}
+        setFormData={setFormData}
+      />
     </div>
   );
 }
