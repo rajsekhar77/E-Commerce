31b046f5925e5398279c5f966d7b58f204e90fe4
rajsekhar77 <rajasekhar.b5401@gmail.com>
2024-09-22 23:00:38 +0530

    created the asyncthunks and builders for products endpoints

---
+++ fronted/src/features/shop/products/products-slice.js
@@ -0,0 +1,68 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+  isLoading: false,
+  productList: [],
+  productDetails: null,
+};
+
+export const fetchAllFilteredProducts = createAsyncThunk(
+  "/products/fetchallproducts",
+  async ({ filterParams, sortParams }) => {
+    const query = new URLSearchParams({ ...filterParams, sortBy: sortParams });
+    // console.log(query.toString(), 'query');
+    const result = await axios.get(
+      `http://localhost:3000/api/shop/products/get?${query}`
+    );
+    return result?.data;
+  }
+);
+
+export const fetchProductDetails = createAsyncThunk(
+  "/products/fetchProductDetails",
+  async (id) => {
+    const result = await axios.get(
+      `http://localhost:3000/api/shop/products/get/${id}`
+    );
+    return result?.data;
+  }
+);
+
+const shopProductsSlice = createSlice({
+  name: "shoppingProducts",
+  initialState,
+  reducers: {
+    setProductDetails: (state) => {
+      state.productDetails = null;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchAllFilteredProducts.pending, (state, action) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchAllFilteredProducts.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.productList = action.payload.data;
+      })
+      .addCase(fetchAllFilteredProducts.rejected, (state, action) => {
+        state.isLoading = false;
+        state.productList = [];
+      })
+      .addCase(fetchProductDetails.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchProductDetails.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.productDetails = action.payload.data;
+      })
+      .addCase(fetchProductDetails.rejected, (state) => {
+        state.isLoading = false;
+        state.productDetails = null;
+      });
+  },
+});
+
+export default shopProductsSlice.reducer;
+export const { setProductDetails } = shopProductsSlice.actions;
