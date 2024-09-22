b558ed5463bac6a22ba6bdd924c3ce350d63e12b
rajsekhar77 <rajasekhar.b5401@gmail.com>
2024-09-22 14:01:28 +0530

    completed the sidebar

--- fronted/src/components/admin-view/Layout.jsx
+++ fronted/src/components/admin-view/Layout.jsx
@@ -1,15 +1,18 @@
 import { Outlet } from "react-router-dom";
 import AdminSidebar from "./AdminSidebar";
 import AdminHeader from "./AdminHeader";
+import { useState } from "react";
 
 function AdminLayout() {
+  const [openSidebar, setOpenSidebar] = useState(false);
+
   return (
     <div className="flex min-h-screen w-full">
       {/* adminSidebar */}
-      <AdminSidebar />
+      <AdminSidebar open={openSidebar} setOpen={setOpenSidebar} />
       <div className="flex flex-1 flex-col">
         {/* adminHeader */}
-        <AdminHeader />
+        <AdminHeader setOpen={setOpenSidebar} />
         <main className="flex-1 flex-col flex bg-muted/40 p-4 md:p-6">
           <Outlet />
         </main>
