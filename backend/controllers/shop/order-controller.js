e39c6a878d769da60cb6e384c5c7828d40636056
rajsekhar77 <rajasekhar.b5401@gmail.com>
2024-09-26 12:01:06 +0530

    getallorderusers and getorderdetails functions are created for order-controller

--- backend/controllers/shop/order-controller.js
+++ backend/controllers/shop/order-controller.js
@@ -148,3 +148,55 @@ export const capturePayment = async (req, res) => {
     });
   }
 };
+
+export const getAllOrdersByUser = async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    const orders = await Order.find({ userId });
+
+    if (!orders.length) {
+      return res.status(404).json({
+        success: false,
+        message: "No Orders found!",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: orders,
+    });
+  } catch (error) {
+    console.log("Error in getAllOrdersByUser: ", error.message);
+    res.status(500).json({
+      success: false,
+      message: "Some Error Occured",
+    });
+  }
+};
+
+export const getOrderDetails = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const order = await Order.findById(id);
+
+    if (!order) {
+      return res.status(404).json({
+        success: false,
+        message: "Order not found!",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: order,
+    });
+  } catch (error) {
+    console.log("Error in getOrderDetails: ", error.message);
+    res.status(500).json({
+      success: false,
+      message: "Some Error Occured",
+    });
+  }
+};
