0e5fbd6b4b00ffd3d786a04555cfb225272f0b48
rajsekhar77 <rajasekhar.b5401@gmail.com>
2024-09-26 11:44:31 +0530

    createOrder function is created for order-controller

---
+++ backend/controllers/shop/order-controller.js
@@ -0,0 +1,96 @@
+import { Cart } from "../../models/Cart.js";
+import { Order } from "../../models/Order.js";
+import { paypal } from "../../paypalConfig/index.js";
+import { Product } from "../../models/Product.js";
+
+export const createOrder = async (req, res) => {
+  try {
+    const {
+      userId,
+      cartId,
+      cartItems,
+      addressInfo,
+      orderStatus,
+      paymentMethod,
+      paymentStatus,
+      totalAmount,
+      orderDate,
+      orderUpdateDate,
+      paymentId,
+      payerId,
+    } = req.body;
+    const create_payment_json = {
+      intent: "sale",
+      payer: {
+        payment_method: "paypal",
+      },
+      redirect_urls: {
+        return_url: "http://localhost:5173/shop/paypal-return",
+        cancel_url: "http://localhost:5173/shop/paypal-cancel",
+      },
+      transactions: [
+        {
+          item_list: {
+            items: cartItems.map((item) => {
+              return {
+                name: item.title,
+                sku: item.productId,
+                price: item.price.toFixed(2),
+                currency: "USD",
+                quantity: item.quantity,
+              };
+            }),
+          },
+          amount: {
+            currency: "USD",
+            total: totalAmount.toFixed(2),
+          },
+          description: "description",
+        },
+      ],
+    };
+
+    paypal.payment.create(create_payment_json, async (error, paymentInfo) => {
+      if (error) {
+        console.log(error);
+        return res.status(500).json({
+          success: false,
+          message: "Error while creating paypal payment",
+        });
+      } else {
+        const newlyCreatedOrder = new Order({
+          userId,
+          cartId,
+          cartItems,
+          addressInfo,
+          orderStatus, // from fronted we pass pending status after success of payment we assign pending
+          paymentMethod,
+          paymentStatus, // same
+          totalAmount,
+          orderDate,
+          orderUpdateDate,
+          paymentId,
+          payerId,
+        });
+
+        await newlyCreatedOrder.save();
+
+        const approvalURL = paymentInfo.links.find((link) => {
+          return link.rel === "approval_url";
+        }).href;
+
+        res.status(201).json({
+          success: true,
+          approvalURL,
+          orderId: newlyCreatedOrder._id,
+        });
+      }
+    });
+  } catch (error) {
+    console.log("Error in createOrder controller: ", error.message);
+    res.stats(500).json({
+      success: false,
+      message: "Error",
+    });
+  }
+};
