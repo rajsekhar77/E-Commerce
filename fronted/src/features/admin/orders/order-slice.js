6c73f7fbbf54060385561346ffc097ceef2ae8c7
rajsekhar77 <rajasekhar.b5401@gmail.com>
2024-10-02 15:53:52 +0530

    created the asyncthunks and builder for admin order endpoints

---
+++ fronted/src/features/admin/orders/order-slice.js
@@ -0,0 +1,78 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+  isLoading: false,
+  orderList: [],
+  orderDetails: null,
+};
+
+export const getAllOrdersForAdmin = createAsyncThunk(
+  "/products/getAllOrdersForAdmin",
+  async () => {
+    const result = await axios.get(
+      `http://localhost:3000/api/admin/orders/get`
+    );
+    return result?.data;
+  }
+);
+
+export const getOrderDetailsForAdmin = createAsyncThunk(
+  "/products/getOrderDetailsForAdmin",
+  async (id) => {
+    const result = await axios.get(
+      `http://localhost:3000/api/admin/orders/details/${id}`
+    );
+    return result?.data;
+  }
+);
+
+export const updateOrderStatus = createAsyncThunk(
+  "/products/updateOrderStatus",
+  async ({ id, orderStatus }) => {
+    const result = await axios.put(
+      `http://localhost:3000/api/admin/orders/update/${id}`,
+      { orderStatus }
+    );
+    return result?.data;
+  }
+);
+
+const adminOrderSlice = createSlice({
+  name: "adminOrderSlice",
+  initialState,
+  reducers: {
+    resetOrderDetails: (state) => {
+      state.orderDetails = null;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getAllOrdersForAdmin.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getAllOrdersForAdmin.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.orderList = action.payload.data;
+      })
+      .addCase(getAllOrdersForAdmin.rejected, (state) => {
+        state.isLoading = false;
+        state.orderList = [];
+      })
+      .addCase(getOrderDetailsForAdmin.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getOrderDetailsForAdmin.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.orderDetails = action.payload.data;
+      })
+      .addCase(getOrderDetailsForAdmin.rejected, (state) => {
+        state.isLoading = false;
+        state.orderDetails = null;
+      });
+  },
+});
+
+export default adminOrderSlice.reducer;
+
+export const { resetOrderDetails } = adminOrderSlice.actions;
