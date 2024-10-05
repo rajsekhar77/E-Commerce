989968580a996dd1e8437e8c6a5128e8c6ae4707
rajsekhar77 <rajasekhar.b5401@gmail.com>
2024-10-05 14:35:21 +0530

    add to cart functionality added for the productdetailsdialog

--- fronted/src/components/shopping-view/ProductDetails.jsx
+++ fronted/src/components/shopping-view/ProductDetails.jsx
@@ -8,21 +8,59 @@ import {
 import { Button } from "../ui/button";
 import { Separator } from "../ui/separator";
 import { useDispatch, useSelector } from "react-redux";
+import { addToCart, fetchCartItems } from "@/features/shop/cart/cart-slice";
 import { useToast } from "@/hooks/use-toast";
 import { setProductDetails } from "@/features/shop/products/products-slice";
 import StarRatingComponent from "../commonComponents/star-rating";
 
 function ProductDetailsDialog({ open, setOpen, productDetails }) {
   const { user } = useSelector((state) => state.auth);
+  const { cartItems } = useSelector((state) => state.shopCart);
 
   const dispatch = useDispatch();
   const { toast } = useToast();
+
+  function handleAddToCart(getCurrentProductId, getTotalStock) {
+    // console.log(getCurrentProductId);
+    let getCartItems = cartItems.items || [];
+
+    if (getCartItems.length) {
+      const indexOfCurrentItem = getCartItems.findIndex(
+        (item) => item.productId === getCurrentProductId
+      );
+
+      if (indexOfCurrentItem > -1) {
+        const getQuantity = getCartItems[indexOfCurrentItem].quantity;
+
+        if (getQuantity + 1 > getTotalStock) {
+          toast({
+            title: `Only ${getQuantity} quantity can be added for this item`,
+            variant: "destructive",
+          });
+          return;
+        }
+      }
+    }
+    dispatch(
+      addToCart({
+        userId: user?.id,
+        productId: getCurrentProductId,
+        quantity: 1,
+      })
+    ).then((data) => {
+      if (data.payload?.success) {
+        dispatch(fetchCartItems(user?.id));
+        toast({
+          title: "Product is added to cart",
+        });
+      }
+    });
+  }
 
   function handleDialogClose() {
     setOpen(false);
     dispatch(setProductDetails());
   }
-
 
   return (
     <Dialog
@@ -66,7 +104,7 @@ function ProductDetailsDialog({ open, setOpen, productDetails }) {
           </div>
           <div className="flex items-center gap-2 mt-2">
             <div className="flex items-center gap-0.5">
-              <StarRatingComponent rating={3}/>
+              <StarRatingComponent rating={3} />
             </div>
             <span className="text-muted-foreground">(3)</span>
           </div>
@@ -77,6 +115,12 @@ function ProductDetailsDialog({ open, setOpen, productDetails }) {
               </Button>
             ) : (
               <Button
+                onClick={() =>
+                  handleAddToCart(
+                    productDetails?._id,
+                    productDetails?.totalStock
+                  )
+                }
                 className="w-full"
               >
                 Add to cart
