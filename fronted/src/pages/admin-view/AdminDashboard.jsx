9049fbd0b0865ebab6cf0124ed02ff4477879586
rajsekhar77 <rajasekhar.b5401@gmail.com>
2024-10-11 20:57:11 +0530

    ui-design completed for the adminDashboard and its functionality

--- fronted/src/pages/admin-view/AdminDashboard.jsx
+++ fronted/src/pages/admin-view/AdminDashboard.jsx
@@ -1,5 +1,65 @@
+import ProductImageUpload from "@/components/admin-view/image-upload";
+import { Button } from "@/components/ui/button";
+import {
+  addFeatureImage,
+  getFeatureImages,
+} from "@/features/common/common-slice";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
 function AdminDashboard() {
-  return <div>AdminDashboard</div>;
+  const [imgaeFile, setImageFile] = useState(null);
+  const [uploadedImageUrl, setUploadedImageUrl] = useState("");
+  const [imageLoadingState, setImageLoadingState] = useState(false);
+
+  const dispatch = useDispatch();
+  const { featureImageList } = useSelector((state) => state.commonFeature);
+
+  function handleUploadFeatureImage() {
+    dispatch(addFeatureImage(uploadedImageUrl)).then((data) => {
+      if (data?.payload?.success) {
+        dispatch(getFeatureImages())
+        setImageFile(null)
+        setUploadedImageUrl('')
+      }
+    });
+  }
+
+  useEffect(() => {
+    dispatch(getFeatureImages());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <ProductImageUpload
+        file={imgaeFile}
+        setFile={setImageFile}
+        uploadedImageUrl={uploadedImageUrl}
+        setUploadedImageUrl={setUploadedImageUrl}
+        setImageLoadingState={setImageLoadingState}
+        imageLoadingState={imageLoadingState}
+        isCustomStyling={true}
+        // isEditMode={currentEditedId !== null}
+      />
+      <Button onClick={handleUploadFeatureImage} className="mt-4">
+        Upload
+      </Button>
+      <div className="flex flex-col gap-4 mt-5">
+        {featureImageList && featureImageList.length > 0
+          ? featureImageList.map((item) => {
+              return (
+                <div className="relative">
+                <img
+                  src={item?.image}
+                  className="w-full h-[300px] object-cover rounded-lg"
+                />
+              </div>
+              );
+            })
+          : null}
+      </div>
+    </div>
+  );
 }
 
 export default AdminDashboard;
